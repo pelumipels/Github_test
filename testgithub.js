74f01e066d49dcb5d2e1fc23734599a7189eb26a
Pelumipels <Pelumi6000@gmail.com>
2025-12-05 20:28:20 +0100

    first commit

---
+++ testgithub.js
@@ -0,0 +1,31 @@
+function calculator(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+
+    case "-":
+      return num1 - num2;
+
+    case "*":
+      return num1 * num2;
+
+    case "/":
+      if (num2 === 0) {
+        return "Error: Division by zero";
+      }
+      return num1 / num2;
+
+    default:
+      return "Invalid operator";
+  }
+}
+
+// console.log(calculator(10, 5, "+")); // 15
+// console.log(calculator(10, 5, "-")); // 5
+// console.log(calculator(10, 5, "*")); // 50
+// console.log(calculator(10, 5, "/")); // 2
+
+console.log(calculator(30, 5, "+"));
+console.log(calculator(30, 5, "-"));
+console.log(calculator(30, 5, "*"));
+console.log(calculator(30, 5, "/"));
